b015ca426b6a3e676c540787ade4a12b1548dd0c
Ade <me@shycoder.com>
2022-08-17 23:52:27 +0100

    Fixed encryption util

--- includes/Utils.php
+++ includes/Utils.php
@@ -6,10 +6,17 @@ defined('ABSPATH') || exit;
 
 class Utils
 {
-    // TODO: Complete sanitizer
-    public static function sanitize_inputs($inputs, $options): array
+    private const SETTINGS_KEY = 'awps_settings_key';
+
+    /**
+     * Sanitize settings input fields
+     * 
+     * @param array $inputs
+     * @param array $options
+     * @return array
+     */
+    public static function sanitize_inputs($inputs, $options, $api_keys): array
     {
-        $options = get_option('awps_options');
         $sanitized_input = [];
 
         foreach ($inputs as $input_key => $input_value) {
@@ -22,7 +29,18 @@ class Utils
                 $input_value = trim(strip_tags(stripslashes($input_value)));
                 $input_value = sanitize_text_field($input_value);
 
-                //$input_value = $this->encrypt_keys($input_value);
+                // encrypt api key fields
+                if (!empty($api_keys)) {
+
+                    // TODO: check call_user_func
+                    foreach ($api_keys as $api_key) {
+
+                        if ($input_key === $api_key) {
+
+                            $input_value = self::encrypt($input_value);
+                        }
+                    }
+                }
 
                 $sanitized_input[$input_key] = $input_value;
             }
@@ -31,21 +49,55 @@ class Utils
         return $sanitized_input;
     }
 
-    public function getKeys()
+    /**
+     * Save a random key for settings field encryption
+     */
+    public static function store_key(): void
     {
-        $secrete_key = hash('sha256', 'kvvzukizqtmztvafoozlovydalgbriyi');
-        $secrete_iv =  substr(hash('sha256', 'kvvzukizqtmztvafoozlovydalgbriyi'), 0, 16);
+        $key = bin2hex(random_bytes(16)); // generate random opensll compatible string
+        add_option(self::SETTINGS_KEY, $key);
     }
 
-    public static function encrypt($key)
+    /**
+     * Encrypt an input
+     * 
+     * @param string
+     */
+    private static function encrypt($key)
     {
+        $encryption_keys = self::get_keys();
 
-        return base64_encode(openssl_encrypt($key, AWPS_ENCRYPTION_METHOD, 'secrete_key', 0, 'secrete_iv'));
+        if (!empty($encryption_keys)) {
+            return base64_encode(openssl_encrypt($key, AWPS_ENCRYPTION_METHOD, $encryption_keys['key'], 0, $encryption_keys['iv']));
+        }
     }
 
+    /**
+     * Decrypt and input
+     * 
+     * @param string
+     */
     public static function decrypt($key)
     {
+        $encryption_keys = self::get_keys();
 
-        return openssl_decrypt(base64_decode($key), AWPS_ENCRYPTION_METHOD, 'secrete_key', 0, 'secrete_iv');
+        if (!empty($keys)) {
+            return openssl_decrypt(base64_decode($key), AWPS_ENCRYPTION_METHOD, $encryption_keys['key'], 0, $encryption_keys['iv']);
+        }
+    }
+
+    private static function get_keys(): array
+    {
+        $option = get_option(self::SETTINGS_KEY);
+
+        if ($option) {
+
+            $secrete_key = hash('sha256', $option);
+            $secrete_iv =  substr(hash('sha256', $option), 0, 16);
+
+            return ['key' => $secrete_key, 'iv' => $secrete_iv];
+        }
+
+        return [];
     }
 }
